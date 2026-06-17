728916d96d454a3243e515b98b855a2600f0e309
hassanadawieh <hassan.adawieh@tedmob.com>
2026-06-17 16:38:26 +0300

    update the api in the next config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${API_URL}/:path*`,
+        destination: `${API_URL}/api/:path*`,
       },
     ];
   },
